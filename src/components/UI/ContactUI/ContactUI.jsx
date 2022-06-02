a590b4f19aaf751cb389395f52db39f968cce29a
ivan <ivanhshs1@gmail.com>
2022-06-02 11:17:20 -0500

    seven commit

--- src/components/UI/ContactUI/ContactUI.jsx
+++ src/components/UI/ContactUI/ContactUI.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import emailjs from '@emailjs/browser'
 
 export const ContactUI = () => {
+
+    const sendEmail = (event) => {
+        event.preventDefault();
+
+        emailjs.sendForm('service_351izji','template_331pfqp', event.target, 'yxX0zL03525FgmUHm')
+        .then(response => console.log(response)).catch(error => console.log(error));
+    }
+
   return (
     <div>
 
@@ -9,7 +17,7 @@ export const ContactUI = () => {
             <div className="cajaContactH2">
                 <h2>Contact Us</h2>
             </div>
-            <form >
+            <form onSubmit={sendEmail}>
                 <div className="cajaInput">
                     <label className="inputContact" >Nombre:</label>
                     <input type="text" className="inputContact" name="nombre"/>
@@ -18,9 +26,10 @@ export const ContactUI = () => {
                     <label className="inputContact" >Mensaje:</label>
                     <textarea type="textarea" className="inputContactArea" name="mensaje"/>
                 </div>
+                <button className="btn-EnviarContact" >ENVIAR</button>
             </form>
 
-            <button className="btn-EnviarContact">ENVIAR</button>
+
         </div>
 
     </div>
